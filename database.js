26fff4cd0d323697fefb6649451f527244dec9f6
rdowd <rjarrettdowd@yahoo.com>
2020-04-08 01:02:10 -0400

    completed all routes mvp complete.

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
-let users = {
+let users = [{
     id: "1", 
     name: "Jane Doe", 
     bio: "Not Tarzan's Wife, another Jane",  
-}
+}]
 
 function createUser(data) {
     const payload = {
@@ -18,6 +18,27 @@ function getUsers() {
     return users
 };
 
+function getUserById(id){
+    return users.find(u => u.id === id);
+}
+
+function deleteUser(id){
+    users = users.filter(u => u.id != id);
+}
+
+function updateUser(id, data) {
+    const index = users.findIndex(u => u.id === id);
+    users[index] = {
+        ...users[index],
+        ...data
+    }
+    return users
+}
+
 module.exports ={
     getUsers,
+    createUser,
+    getUserById,
+    deleteUser,
+    updateUser
 }
--- index.js
+++ index.js
@@ -41,6 +41,75 @@ server.get("/api/users", (req, res) => {
     
 })
 
+server.get("/api/users/:id", (req, res) => {
+    const userId = req.params.id;
+    const user = db.getUserById(userId);
+
+    if(!user){
+        res.status(404).json({
+            message: "The user with the specified ID does not exist." 
+        })
+    }else{
+        if(user){
+            res.status(200).json(user);
+        }else{
+            res.status(500).json({
+                errorMessage: "The user information could not be retrieved."
+            })
+        }
+    }
+})
+
+server.delete("/api/users/:id", (req, res) =>{
+    const userId = req.params.id;
+    const user = db.getUserById(userId);
+
+    if(!user){
+        res.status(404).json({
+            message: "The user with the specified ID does not exist." 
+        })
+    }else{
+        const userDeleted = db.deleteUser(userId);
+        if(userDeleted){    
+            res.status(204).end();
+        }else{
+            res.status(500).json({
+                errorMessage: "The user information could not be removed."
+            })
+        }
+    }
+    
+})
+
+server.put("/api/users/:id", (req, res) => {
+    const user = db.getUserById(req.params.id);
+
+    if(!user){
+        res.status(404).json({
+            message: "The user with the specified ID does not exist." 
+        })
+    }else if(!req.body.name || !req.body.bio){
+        res.status(400).json({
+            errorMessage: "Please provide name and bio for the user." 
+        })
+    }else{
+        const updateUser = db.updateUser(user.id,{
+            name: req.body.name,
+            bio: req.body.bio
+        })
+        console.log(user);
+        if(updateUser){
+           res.status(200).json(updateUser); 
+        }else{
+            res.status(500).json({
+                errorMessage: "The user information could not be modified." 
+            })
+        }
+        
+    }
+
+})
+
 server.listen(5000, () =>{
     console.log("Server is listening on port 5000");
 } )
